67873a982f183c193c34efb2abe2d51d01c91a57
Vitor223 <vitor.costa@sptech.school>
2023-09-08 23:10:51 -0300

    card agenda finalizado

--- site-institucional-react/src/pages/agenda.jsx
+++ site-institucional-react/src/pages/agenda.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import Menu from '../components/menu/Menu_logado.jsx'
 import Footer from '../components/footer/footer.jsx'
+import Card from '../components/cards/card_agenda.jsx'
 import '../css-images/css/agenda.css'
+import CardMarcado from '../components/cards/card_agenda_marc.jsx'
+import Julia from '../css-images/img/meninona.png'
 
 
 function Agenda() {
@@ -12,6 +15,20 @@ function Agenda() {
         <span className="tituloAgenda">
         Esses são os treinos agendados com você
         </span>
+        <div className="seguraCard">
+          <div className="duplaCard">
+            <Card img={Julia} nome='Julia Silva' dataTreino='Data: 23/04/2023' horaTreino='14:00' localTreino='Academia Salvador'></Card>
+            <CardMarcado img={Julia} nome='Julia Silva' dataTreino='Data: 23/04/2023' horaTreino='14:00' localTreino='Academia Salvador'></CardMarcado>
+          </div>
+          <div className="duplaCard">
+            <Card img={Julia} nome='Julia Silva' dataTreino='Data: 23/04/2023' horaTreino='14:00' localTreino='Academia Salvador'></Card>
+            <CardMarcado img={Julia} nome='Julia Silva' dataTreino='Data: 23/04/2023' horaTreino='14:00' localTreino='Academia Salvador'></CardMarcado>
+          </div>
+          <div className="duplaCard">
+            <Card img={Julia} nome='Julia Silva' dataTreino='Data: 23/04/2023' horaTreino='14:00' localTreino='Academia Salvador'></Card>
+            <CardMarcado img={Julia} nome='Julia Silva' dataTreino='Data: 23/04/2023' horaTreino='14:00' localTreino='Academia Salvador'></CardMarcado>
+          </div>
+        </div>
     </div>
     <Footer/>
     </>
