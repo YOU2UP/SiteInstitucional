1fc4ddb75016d97413ab0bdaba365f526dc48dbf
Vitor223 <vitor.costa@sptech.school>
2023-10-14 20:33:55 -0300

    atualizacao

--- site-institucional-react/src/components/comp_config/meta.jsx
+++ site-institucional-react/src/components/comp_config/meta.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import api from '../../api'
+import '../../css-images/css/meta.css'
 
 function Meta() {
 
@@ -14,15 +15,20 @@ function Meta() {
 
   const [meta, setMeta] = useState(0)
 
+
+
   useEffect(() => {
     api.get(`/usuarios/${id}`, config).then((response) => {
       setMeta(response.data.metaTreinos)
-      
+      console.log(token)
       console.log("meta de treinos: ", meta)
     }).catch((error) => {
       console.log("Erro: ", error)
-    })
+    })  
+  
   }, [])
+
+
 
 
   return (
@@ -30,7 +36,22 @@ function Meta() {
       <>
 
         <div className="containerMeta">
-          <span className="tituloMeta">Definir sua Meta</span>
+          <div className='textoMeta'>
+          <span className="tituloMeta">Defina sua Meta</span>
+             Sua meta atual é...
+
+             {meta === 0 ? (
+              <span className="metaAtual">Você ainda não possui uma meta... defina uma!</span>
+            ) : (
+              <span className="metaAtual">{meta} treinos</span>
+            )}
+
+          </div>
+
+          <div className="defineMeta">
+          O que você gostaria de alcançar?
+          <input type="text" name='meta' placeholder='Ex: Eu gostaria de alcançar 150 treinos' className='iptMeta' />
+          </div>
 
         </div>
       </>
